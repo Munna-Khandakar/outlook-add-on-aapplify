57b290ae4381e6d76c0a05847eb1240aa5592ddf
Munna-Khandakar <munnashisad@gmail.com>
2024-09-30 14:46:32 +0600

    btn shadow rmv

--- src/components/EmailPrompt.tsx
+++ src/components/EmailPrompt.tsx
@@ -45,12 +45,12 @@ export const EmailPrompt = (props: EmailPromptProps) => {
             <div className="flex justify-end">
                 <div className="relative group">
                     <button
-                        className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-300 dark:bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95"
+                        className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-300 dark:bg-gray-800 cursor-pointer rounded-lg shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95"
                     >
                             <span
                                 className="absolute inset-0 rounded-lg bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
-                        <span className="relative z-10 block px-3 py-1 rounded-lg bg-gray-50 dark:bg-gray-700">
-        <div className="relative z-10 flex items-center space-x-2">
+                        <span className="relative block px-3 py-1 rounded-lg bg-gray-50 dark:bg-gray-700">
+        <div className="relative flex items-center space-x-2">
 
             {
                 isSubmitting
@@ -61,7 +61,7 @@ export const EmailPrompt = (props: EmailPromptProps) => {
                         <span
                             className="transition-all duration-500 group-hover:translate-x-1 text-sm text-gray-900 dark:text-white">Generate Email</span>
                         <svg
-                            className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                            className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1 text-gray-900 dark:text-white"
                             data-slot="icon"
                             aria-hidden="true"
                             fill="currentColor"
